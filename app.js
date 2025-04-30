e2aa5c9dfef1570f31eef5fd5009559fc877c387
usncahill <usncahill@hotmail.com>
2025-04-30 00:19:05 -0400

    various auth changes

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-//const fs = require('fs');
-//const https = require('https');
-//const path = require('path');
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 3001;
 
 //Tesla specific
 const url_auth_endpoint = new URL("https://fleet-auth.prd.vn.cloud.tesla.com/oauth2/v3/authorize");
+const url_token_endpoint = new URL("https://fleet-auth.prd.vn.cloud.tesla.com/oauth2/v3/token");
+
 app.get('/.well-known/appspecific/:name', (req, res, next) => {
     const options = {
         root: "./",
@@ -40,14 +40,38 @@ app.get('/auth', (req, res) => {
     };
   
     const searchparams = new URLSearchParams(paramsObj);
-    
     res.redirect(url_auth_endpoint + "?" + searchparams.toString());
 })
 
 app.get('/auth/callback', (req, res, next) => {
-  
-    res.send(req.body);
+    callback_code=req.query.code.toString() || next(err);
+    
+    console.log(callback_code);
+    
+    const paramsObj = {
+        'client_id': 'a1b5658c-14f8-4685-9cee-5cb597476b62',
+        'client_secret': process.env.CLIENT_SECRET,
+        'grant_type': 'authorization_code',
+        'code': callback_code,
+        'redirect_uri': 'https://mmm-t3-service.onrender.com/auth/token',
+        'audience': 'https://fleet-api.prd.na.vn.cloud.tesla.com'
+    }
+    
+    const searchparams = new URLSearchParams(paramsObj);
+    
+    //res.send(url_token_endpoint + "?" + searchparams.toString());
+    res.redirect(url_token_endpoint + "?" + searchparams.toString());
 });
+
+app.get('/auth/token', (req, res, next) => {
+    
+    refresh_token=req.query.refresh_token.toString() || next(err);
+    access_token=req.query.access_token.toString() || next(err);
+    
+    res.send("refresh_token: " + refresh_token + "\n" + 
+             "access_token: " + access_token );
+});
+
 
 const server = app.listen(port, function() {
     console.log("Listening on " + port);
