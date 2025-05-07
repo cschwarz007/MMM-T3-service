dfc7d165aff110aa8bf502570045b07e471241d4
usncahill <usncahill@hotmail.com>
2025-05-06 23:20:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const port = process.env.PORT || 3001;
 const https = require("https");
 
 //Tesla specific
-const url_auth = "https://fleet-auth.prd.vn.cloud.tesla.com";
-const url_data = "https://fleet-api.prd.na.vn.cloud.tesla.com";
+const urlAuth = "https://fleet-auth.prd.vn.cloud.tesla.com";
+const urlData = "https://fleet-api.prd.na.vn.cloud.tesla.com";
 
 app.get('/.well-known/appspecific/:name', (req, res, next) => {
     const options = {
@@ -30,8 +30,16 @@ app.get('/.well-known/appspecific/:name', (req, res, next) => {
 })
 
 app.get('/auth', (req, res) => {
+    if (!req.query["password"]) {
+        res.send("Internal server error");
+        return;
+    } else if (req.query["password"] !== process.env.PASSWORD) {
+        res.send("Internal server error");
+        return;
+    }
+
     //Tesla requires this to be query
-    const paramsObj = {
+    const paramsObj = new URLSearchParams({
         'client_id': process.env.CLIENT_ID,
         'scope': 'openid vehicle_device_data vehicle_cmds offline_access',
         'locale': 'en-US',
@@ -39,10 +47,9 @@ app.get('/auth', (req, res) => {
         'redirect_uri': 'https://' + req.get('host') + '/auth/callback',
         'response_type': 'code',
         'state': (Math.floor(Math.random() * 10))
-    }
+    });
   
-    const searchparams = new URLSearchParams(paramsObj);
-    res.redirect(url_auth + '/oauth2/v3/authorize' + "?" + searchparams.toString());
+    res.redirect(urlAuth + '/oauth2/v3/authorize' + "?" + paramsObj.toString());
 })
 
 app.get('/auth/callback', (req, res) => {
@@ -52,49 +59,43 @@ app.get('/auth/callback', (req, res) => {
         if (req.query["refresh_token"]) { refresh_token=req.query.refresh_token.toString() }
         if (req.query["access_token"]) { access_token=req.query.access_token.toString() }
         
-        res.send(req.body + "\n\n" + 
+        res.send(JSON.stringify(req) + "\n\n" + 
              "refresh_token: " + refresh_token + "\n" + 
              "access_token: " + access_token );
     }
     
-    const paramsObj = {
+    const paramsObj = new URLSearchParams({
         'grant_type': 'authorization_code',
         'client_id': process.env.CLIENT_ID,
         'client_secret': process.env.CLIENT_SECRET,
         'code': callback_code,
         'redirect_uri': 'https://' + req.get('host') + '/auth/callback',
-        'audience': url_data
-    }
+        'audience': urlData
+    });
     
     options = {
         method: 'POST',
-        host: url_auth + '/oauth2/v3/token',
-        headers: { 'content-type': 'text/json' },
-        body: JSON.stringify(paramsObj)
+        host: urlAuth,
+        path: '/oauth2/v3/token',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        body: paramsObj.toString().replace("\*","%2A")
     };
     
-    const newreq = https.request(options, (res) => {
+    const newreq = https.request(options, (newres) => {
         let data = '';
 
-        res.on('data', (chunk) => {
+        newres.on('data', (chunk) => {
             data += chunk;
         });
 
         // Ending the response 
-        res.on('end', () => {
+        newres.on('end', () => {
             console.log('Body:', JSON.parse(data))
         });
     }).on("error", (err) => {
         console.log("Error: ", err)
     }).end()
-    
-    //const searchparams = new URLSearchParams(paramsObj);
-    //res.redirect(url_auth + '/oauth2/v3/token'); // + "?" + searchparams.toString().replace("\*","%2A"));
-});                    
-
-//app.get('/',(req,res) => {
-//    res.send(html);
-//});
+});
 
 const server = app.listen(port, function() {
     console.log("Listening on " + port);
@@ -102,17 +103,3 @@ const server = app.listen(port, function() {
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hey Dane!</title>
-  </head>
-  <body>
-    <section>
-      Totally functional web page, nothing to see here...
-    </section>
-  </body>
-</html>
-`
